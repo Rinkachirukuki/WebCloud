6e56b128b526faa2f39993c4dfe6a85fa85e88b4
Rinkachirukuki <55659018+Rinkachirukuki@users.noreply.github.com>
2020-08-17 21:40:07 +0500

    files page

---
+++ src/main/java/com/itrev/WebCloud/Controllers/FileController.java
@@ -0,0 +1,13 @@
+package com.itrev.WebCloud.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FileController {
+    @GetMapping("/Files")
+    public String Files(Model model){
+    return "Files";
+    }
+}
