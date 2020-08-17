bacc3095ae28f7d145ef09fa6447ac899260d53a
Rinkachirukuki <55659018+Rinkachirukuki@users.noreply.github.com>
2020-08-17 16:03:45 +0500

    creating

---
+++ src/test/java/com/itrev/WebCloud/WebCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itrev.WebCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
