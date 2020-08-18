a3f8b25c46132b585dacc2274e4b68100f3d832b
Rinkachirukuki <55659018+Rinkachirukuki@users.noreply.github.com>
2020-08-18 15:15:49 +0500

    unstable

--- src/main/java/com/itrev/WebCloud/Controllers/FileController.java
+++ src/main/java/com/itrev/WebCloud/Controllers/FileController.java
@@ -1,13 +1,21 @@
 package com.itrev.WebCloud.Controllers;
 
+import com.itrev.WebCloud.Models.Item;
+import com.itrev.WebCloud.Repo.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FileController {
+    @Autowired
+    private ItemRepository itemRepository;
+
     @GetMapping("/Files")
     public String Files(Model model){
+        Iterable<Item> items = itemRepository.findAll();
+        model.addAttribute("itemss",items);
     return "Files";
     }
 }
---
+++ src/main/java/com/itrev/WebCloud/Repo/ItemRepository.java
@@ -0,0 +1,8 @@
+package com.itrev.WebCloud.Repo;
+
+import com.itrev.WebCloud.Models.Item;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemRepository extends CrudRepository<Item,Long> {
+
+}
