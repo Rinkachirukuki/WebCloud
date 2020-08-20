5e9f31960823c273eaa165f869c8633ea8cbffba
Matthew <andersonlondon18@gmail.com>
2020-08-20 12:28:26 +0500

    Изменения на 20.08.20

--- src/main/java/com/itrev/WebCloud/Controllers/MainController.java
+++ src/main/java/com/itrev/WebCloud/Controllers/MainController.java
@@ -3,7 +3,9 @@ package com.itrev.WebCloud.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class MainController {
@@ -13,5 +15,10 @@ public class MainController {
 		model.addAttribute("title", "Главная страница");
 		return "home";
 	}
+        @PostMapping("/")
+        public String add_file(@RequestParam("file") MultipartFile file){
+            
+            return "home";
+        }
 
 }
--- src/main/java/com/itrev/WebCloud/Models/Item.java
+++ src/main/java/com/itrev/WebCloud/Models/Item.java
@@ -4,13 +4,67 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    private String title;
+    private long id; //id для базы данных
+    private String title; //имя файла
+    private String type;// тип файла
+    private long size; //размер файла
+    private Date upload_date;//дата загрузки
+    private Date change_date;//дата изменения
+    private byte[] file; //содержимое файла
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public Date getUpload_date() {
+        return upload_date;
+    }
+
+    public void setUpload_date(Date upload_date) {
+        this.upload_date = upload_date;
+    }
+
+    public Date getChange_date() {
+        return change_date;
+    }
+
+    public void setChange_date(Date change_date) {
+        this.change_date = change_date;
+    }
+
+    public byte[] getFile() {
+        return file;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
 
     public String getTitle() {
         return title;
