65eb92e0c34a13995cbad1abdc8f1ccb20fc0e5d
Rinkachirukuki <55659018+Rinkachirukuki@users.noreply.github.com>
2020-08-20 14:24:37 +0500

    ded

--- src/main/java/com/itrev/WebCloud/Controllers/FileController.java
+++ src/main/java/com/itrev/WebCloud/Controllers/FileController.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Optional;
 
 @Controller
 public class FileController {
@@ -17,5 +22,13 @@ public class FileController {
         Iterable<Item> items = itemRepository.findAll();
         model.addAttribute("items",items);
     return "Files";
+    }
+    @GetMapping("/{id}")
+    public String FileInfo(@PathVariable(value = "id") Long id, Model model){
+        Optional<Item> item = itemRepository.findById(id);
+        ArrayList<Item> res = new ArrayList<>();
+        item.ifPresent(res::add);
+        model.addAttribute("File", res);
+        return "FileInfo";
     }
 }
